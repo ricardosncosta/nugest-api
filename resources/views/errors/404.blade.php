b899b2d3494d63d764e100518dc09a10cb487ead
Ricardo Costa <miltonrsnc@gmail.com>
2015-10-17 20:06:46 +0100

    Added custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('title', 'Error: 404 not found')
+@section('page_title', 'Error: 404 not found')
+@section('content')
+	The item you are looking for could not be found.
+@endsection
