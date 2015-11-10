bc6b0c5a046e675b7f78957d8e42a2f4a339fffa
Ricardo Costa <miltonrsnc@gmail.com>
2015-11-10 00:16:10 +0000

    Changed class name to match file name

--- database/migrations/2015_10_14_184909_create_dishes_table.php
+++ database/migrations/2015_10_14_184909_create_dishes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Dish extends Migration
+class CreateDishesTable extends Migration
 {
     /**
      * Run the migrations.
